a79d0e296e5f30320b6189b1446047be0378b5a2
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2018-07-24 19:26:49 +0300

    Create noTriples.java

---
+++ noTriples.java
@@ -0,0 +1,11 @@
+# Warmup-2
+Google Inside Look/ CodingBat challenges
+
+public boolean noTriples(int[] nums) {
+  
+  for(int i=0;i<nums.length-2;i++)
+  {
+  if(nums[i]==nums[i+1]&&nums[i]==nums[i+2])return false;  
+  }
+  return true;
+}
